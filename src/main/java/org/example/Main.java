23f19b63c90673a3888adfedf1a4936693e428ca
berkayyyilmaz <yilmaz.berkayyagiz@gmail.com>
2025-02-16 15:41:13 +0300

    Completed.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,42 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+
+
     }
 
+    public static boolean checkForPalindrome(String word) {
+        String editedWord = word.toLowerCase(Locale.ENGLISH).replaceAll("[^a-zA-Z0-0]", "");
+        String reversed = new StringBuilder(editedWord).reverse().toString();
+        System.out.println(editedWord);
+        System.out.println(reversed);
+
+        if (editedWord.equals(reversed)){
+            return Boolean.TRUE;
+        }
+        else {
+            return Boolean.FALSE;
+        }
+    }
+
+    public static String convertDecimalToBinary(int decimal){
+        LinkedList<Integer> binaryList =  new LinkedList<>();
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            binaryList.addFirst(remainder);
+            decimal = decimal / 2 ;
+        }
+        StringBuilder binaryString  = new StringBuilder();
+        for (int digit: binaryList){
+            binaryString.append(digit);
+        }
+        return binaryString.toString();
+    }
 }
